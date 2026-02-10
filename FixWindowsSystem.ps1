cfd8231f9ea270ce7085fc74fac0e30d6178e9e2
0xNixxy <NicholasHoZY@gmail.com>
2026-02-10 10:54:17 +0800

    Sync powershell logic to batch version

--- FixWindowsSystem.ps1
+++ FixWindowsSystem.ps1
@@ -1,9 +1,11 @@
-﻿# Repo: https://github.com/0xNixxy/fix-windows-system-powershell
+﻿# Fix Windows System by 0xNixxy
+# Repo: https://github.com/0xNixxy/fix-windows-system
+# This script needs Windows PowerShell Administrator to execute.
 
 # ==============================================================================
 # MIT License
 #
-# Copyright (c) 2023-2025 Nicholas Ho
+# Copyright (c) 2025-2026 Nicholas Ho
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -32,83 +34,102 @@
 # You can check your current policy with "Get-ExecutionPolicy -List"
 # ==============================================================================
 
-# This script needs PowerShell(Admin) to execute.
-# Check for Admin rights and quit script otherwise.
+
+
+# Check for Admin rights and quit script otherwise
+
 $currID = [System.Security.Principal.WindowsIdentity]::GetCurrent()
 $currPrincipal = new-object System.Security.Principal.WindowsPrincipal(${currID})
 $adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
-if($currPrincipal.IsInRole(${adminRole}))
+
+if(-not $currPrincipal.IsInRole(${adminRole}))
 {
-    # Show version number
-    Write-Output -InputObject "Fix Windows System by 0xNixxy v1.0.1"
+    Write-Warning -Message  "[INFO]    This script requires administrative privileges."
+    Write-Warning -Message  "[INFO]    Please run `"Windows PowerShell`" as Administrator."
+    return
+}
+
+
+
+# Show program name and version
+
+Write-Output -InputObject ""
+Write-Output -InputObject "------------------------------------"
+Write-Output -InputObject "Fix Windows System v1.1.0 by 0xNixxy"
+Write-Output -InputObject "------------------------------------"
+Write-Output -InputObject ""
+
+
+
+# Remove unused driver packages in Windows
+
+Write-Output -InputObject "[INFO]     Clean up unused drivers in Windows system..."
+& rundll32.exe pnpclean.dll,RunDLL_PnpClean /DRIVERS /MAXCLEAN
+Write-Output -InputObject ""
+
+
+
+# Detect and repair errors found in Windows component store (WinSxS)
+
+Write-Output -InputObject "[INFO]     Quick status check on Windows component store health..."
+Repair-WindowsImage -Online -CheckHealth
+Write-Output -InputObject ""
+
+Write-Output -InputObject "[INFO]     Scan Windows component store and automatically fix issues..."
+Repair-WindowsImage -Online -RestoreHealth
+Write-Output -InputObject ""
+
+
+
+# Detect and repair errors found in Windows system files
+# As SFC uses Windows image as a baseline to repair corrupted system files,
+# run SFC only after DISM verified that Windows image is healthy. I found that
+# adopting a trivial approach to run SFC twice per run seems sufficient to
+# resolve all file system issues for virtually all users. In the rare scenario
+# that two SFC runs is unable to fix all issues, the user just needs to
+# execute "SFC /scannow" manually after the script has completed.
+
+Write-Output -InputObject "[INFO]    Check Windows system files and automatically fix issues..."
+Write-Output -InputObject "[INFO]    Run 1 of 2."
+& sfc.exe /scannow
+Write-Output -InputObject ""
+
+Write-Output -InputObject "[INFO]    Run 2 of 2."
+& sfc.exe /scannow
+Write-Output -InputObject ""
+
+
+
+# Remove superseded versions of updated components in Windows component store
+
+Write-Output -InputObject "[INFO]     Clean up unused components in Windows component store..."
+Repair-WindowsImage -Online -StartComponentCleanup
+Write-Output -InputObject ""
+
+
+
+# Detect and repair errors found in disk volume
+
+# Check for errors, and invoke offline repairs if errors detected,
+# otherwise proceed to defrag/trim disk drive.
+Write-Output -InputObject "[INFO]     Checking disk volume..."
+
+if ((Repair-Volume -DriveLetter C -Scan).value__ -eq 0)
+{
+    Write-Output -InputObject ""
+    Write-Output -InputObject "[INFO]     No issues found. Disk volume is healthy."
     Write-Output -InputObject ""
 
-    # ===========================================
-    # DETECT AND REPAIR ERRORS IN COMPONENT STORE
-    # ===========================================
-    # This cmdlet is equivalent to "DISM /Online /Cleanup-Image /CheckHealth",
-    # "DISM /Online /Cleanup-Image /ScanHealth" and
-    # "DISM /Online /Cleanup-Image /RestoreHealth".
-
-    # Check for errors, and perform repairs if errors detected
-    Write-Output -InputObject "🔍 Checking Windows Component Store..."
-    Repair-WindowsImage -Online -CheckHealth
-
-    if ((Repair-WindowsImage -Online -ScanHealth).ImageHealthState -ne "Healthy")
-    {
-        Write-Warning -Message "⚠️ Errors found in Windows Component Store."
-        Write-Output -InputObject "🔧 Repairing Windows Component Store..."
-        Repair-WindowsImage -Online -RestoreHealth
-        Write-Output -InputObject  "✅ Windows Component Store health restored."
-    }
-    else
-    {
-        Write-Output -InputObject "✅ No issues found. Windows Component Store is healthy."
-    }
-
-
-    # ================================================
-    # DETECT AND REPAIR ERRORS IN WINDOWS SYSTEM FILES
-    # ================================================
-    # Run "sfc" to fix and repair issues in Windows system files.
-    # Usually running sfc twice is sufficient to resolve all issues.
-    Write-Output -InputObject "`n🔄 Run 1 of 2: Checking Windows system files..."
-    sfc.exe /scannow
-    Write-Output -InputObject "`n🔄 Run 2 of 2: Checking Windows system files..."
-    sfc.exe /scannow
-
-
-    # =======================================
-    # DETECT AND REPAIR ERRORS IN DISK VOLUME
-    # =======================================
-    # This cmdlet is equivalent to "chkdsk" in Command Prompt.
-
-    # Check for errors, and invoke offline repairs if errors detected,
-    # otherwise proceed to defrag/trim disk drive.
-    Write-Output -InputObject "`n🔍 Checking disk volume..."
-    if ((Repair-Volume -DriveLetter C -Scan).value__ -eq 0)
-    {
-        Write-Output -InputObject "✅ No issues found. Disk volume is healthy."
-
-        # =============================
-        # OPTIMIZE DISK I/O PERFORMANCE
-        # =============================
-        # Improve disk I/O performance of specified volume by performing either
-        # defragmentation on HDD or TRIM on SSD. Optimize-Volume automatically
-        # detects disk type and applies appropriate optimisation technique.
-        # This cmdlet is equivalent to "defrag" in Command Prompt.
-        Write-Output -InputObject   "🧹 Optimising disk volume to improve performance and storage efficiency..."
-        Optimize-Volume -DriveLetter C -Verbose
-        Write-Output -InputObject "✅ Disk volume optimisation complete."
-    }
-    else
-    {
-        Write-Warning -Message "⚠️ Errors found in disk volume."
-        Write-Output -InputObject "🔧 Taking disk volume offline for repairs..."
-        Repair-Volume -DriveLetter C -OfflineScanAndFix
-    }
+    Write-Output -InputObject   "[INFO]     Optimise disk I/O performance and storage efficiency...."
+    Optimize-Volume -DriveLetter C -Verbose
+    Write-Output -InputObject "[INFO]     Disk volume optimisation complete."
+    Read-Host "Press Enter to continue..."
 }
 else
 {
-    Write-Warning -Message  "🔐 This script requires administrative privileges. Please run PowerShell Terminal as Administrator."
+    Write-Output -InputObject ""
+    Write-Warning -Message "[WARNING]  !! Errors found in disk volume."
+    Write-Output -InputObject "[INFO]     Taking disk volume offline for repairs..."
+    Read-Host "Press Enter to continue..."
+    Repair-Volume -DriveLetter C -OfflineScanAndFix
 }
